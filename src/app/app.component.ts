3dcd602601f334e937fcb442a75e14bb80ce0dd1
gstech8f6r2 <gstech8f6r2@gmail.com>
2020-07-12 12:28:42 +0200

    btn Counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'primaApp app Workss!';
   prova = 'ProVaaaaaaaaaaa';
+
+  counter=0;
+  increment(){
+    this.counter++;
+  }
 }
